8939143b53668a4896d7de61164f250bfdc73cc3
Adrianna Gryglak <adrianna.juda@live.com>
2020-01-18 17:58:59 +0100

    Add js that choose game moves

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
-var computerMove, playerMove;
-computerMove = 'kamień';
-playerMove = ' papier';
-printMessage(('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ' -wygrałeś :)'));
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else if (randomNumber == 2) {
+  computerMove = 'papier';
+} else {
+  computerMove = 'nożyczki';
+}
+printMessage('Mój ruch: ' + computerMove);
